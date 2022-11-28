14908deb706fa520be987b9095e045e8edffce79
Andy Chow <andychchow326@gmail.com>
2022-11-29 01:50:48 +0800

    Embed Migrator into DatabaseClient

--- src/main/java/client/DatabaseClient.java
+++ src/main/java/client/DatabaseClient.java
@@ -1,22 +1,22 @@
 package client;
 
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import config.Config;
+import migrator.Migrator;
 
 public class DatabaseClient {
     public Connection connection;
+    public Migrator migrator;
 
-    public DatabaseClient(Config config) throws SQLException {
-        try {
-            this.connection = DriverManager.getConnection(
-                    "jdbc:" + config.DatabaseURL,
-                    config.DatabaseUser,
-                    config.DatabasePassword);
-        } catch (SQLException e) {
-            throw e;
-        }
+    public DatabaseClient(Config config) throws SQLException, FileNotFoundException {
+        this.connection = DriverManager.getConnection(
+                "jdbc:" + config.DatabaseURL,
+                config.DatabaseUser,
+                config.DatabasePassword);
+        this.migrator = new Migrator(this);
     }
 
     public Connection getConnection() {
