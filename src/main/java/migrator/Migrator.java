6cf9521fc1d3706a9bc47038666360beb679bf10
Andy Chow <andychchow326@gmail.com>
2022-11-29 01:44:23 +0800

    Introduce Migrator.java for creating migration on database

---
+++ src/main/java/migrator/Migrator.java
@@ -0,0 +1,119 @@
+package migrator;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import client.DatabaseClient;
+
+public class Migrator {
+    public final static String SCHEMA_FILENAME = "schema.sql";
+    public final static char QUERY_SEPARATOR = ';';
+    private BufferedReader schemaReader;
+    private DatabaseClient dbClient;
+
+    public Migrator(DatabaseClient dbClient) throws FileNotFoundException {
+        this.dbClient = dbClient;
+
+        InputStream input = getClass().getClassLoader().getResourceAsStream(SCHEMA_FILENAME);
+        if (input == null) {
+            throw new FileNotFoundException("Error loading schema file");
+        }
+        this.schemaReader = new BufferedReader(new InputStreamReader(input));
+    }
+
+    public static enum MigrationType {
+        UP {
+            public String getKeyString() {
+                return "-- +migrate Up";
+            }
+        },
+        DOWN {
+            public String getKeyString() {
+                return "-- +migrate Down";
+            }
+        };
+
+        public abstract String getKeyString();
+
+        public boolean isKeyString(String s) {
+            for (MigrationType type : MigrationType.values()) {
+                if (type.getKeyString().equals(s)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public boolean isLineValid(String s) {
+            if (isKeyString(s)) {
+                return getKeyString().equals(s);
+            }
+            return true;
+        }
+    }
+
+    public void up() throws SQLException, IOException {
+        System.out.print("Processing...");
+        this.migrate(MigrationType.UP);
+        System.out.println("Done! Database is initialized!");
+    }
+
+    public void down() throws SQLException, IOException {
+        System.out.print("Processing...");
+        this.migrate(MigrationType.DOWN);
+        System.out.println("Done! Database is removed!");
+    }
+
+    private Statement loadSchema(MigrationType type) throws SQLException, IOException {
+        Statement statement;
+        String line;
+        StringBuffer query = new StringBuffer();
+        boolean isHandlingInvalidLine = false;
+
+        statement = this.dbClient.getConnection().createStatement();
+        while ((line = this.schemaReader.readLine()) != null) {
+            if (isHandlingInvalidLine) {
+                if (type.isKeyString(line) && type.getKeyString().equals(line)) {
+                    isHandlingInvalidLine = false;
+                }
+                continue;
+            }
+            if (!type.isLineValid(line)) {
+                isHandlingInvalidLine = true;
+                continue;
+            }
+            if (this.isCommentLine(line)) {
+                continue;
+            }
+
+            query.append(line);
+            if (this.isQueryEnd(line)) {
+                statement.addBatch(query.toString());
+                query.setLength(0);
+            }
+        }
+
+        return statement;
+    }
+
+    private int[] migrate(MigrationType type) throws SQLException, IOException {
+        Statement statement = this.loadSchema(type);
+        int[] updateCounts = statement.executeBatch();
+        return updateCounts;
+    }
+
+    // Currently only supports two kinds of comment patterns
+    private boolean isCommentLine(String line) {
+        return line.startsWith("--") || line.startsWith("#");
+    }
+
+    // The query should be ended if the string contains the query separator
+    private boolean isQueryEnd(String line) {
+        return line.indexOf(QUERY_SEPARATOR) != -1;
+    }
+}
