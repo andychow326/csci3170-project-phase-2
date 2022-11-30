e3a7106d9065058f8e5417dac844613119d4cb0d
Andy Chow <andychchow326@gmail.com>
2022-11-30 23:32:20 +0800

    Init ManagerOperation

--- src/main/java/salessystem/ManagerOperation.java
+++ src/main/java/salessystem/ManagerOperation.java
@@ -2,7 +2,6 @@ package salessystem;
 
 import java.sql.SQLException;
 import java.io.IOException;
-import java.util.Scanner;
 
 import client.DatabaseClient;
 
@@ -15,6 +14,78 @@ public class ManagerOperation extends BaseOperation {
     }
 
     public void start() {
+        displayManagerMenu();
+    }
 
+    private void displayManagerMenu() {
+        boolean isExit = false;
+        while (!isExit) {
+            System.out.println("\n-----Operations for manager menu----");
+            System.out.println("What kinds of operation would you like to perform?");
+            System.out.println("1. List all salespersons");
+            System.out.println(
+                    "2. Count the no. of sales record of each salesperson under a specific range on years of experience");
+            System.out.println("3. Show the total sales value of each manufacturer");
+            System.out.println("4. Show the N most popular parts");
+            System.out.println("5. Return to the main menu");
+
+            try {
+                isExit = selectOperation();
+            } catch (SQLException e) {
+                System.out.println("Error excecuting SQL query");
+                System.out.println("Error code: " + e.getErrorCode());
+                System.out.println("SQL state: " + e.getSQLState());
+                System.out.println("Message: " + e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.out.println("I/O Error");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean selectOperation() throws SQLException, IOException {
+        System.out.print("Enter Your Choice: ");
+        boolean isExit = false;
+
+        int choice = getInputOption();
+        if (choice < 0) {
+            return isExit;
+        }
+
+        switch (choice) {
+            case 1:
+                listAllSalespersonsByExperience();
+                break;
+            case 2:
+                countTransactionRecordsByExperience();
+                break;
+            case 3:
+                showTotalSalesValueOfEachManufacturer();
+                break;
+            case 4:
+                showNMostPopularParts();
+                break;
+            case 5:
+                isExit = true;
+                break;
+
+            default:
+                System.out.println("ERROR!! Input must be within 1 to 5!");
+        }
+
+        return isExit;
+    }
+
+    private void listAllSalespersonsByExperience() throws SQLException, IOException {
+    }
+
+    private void countTransactionRecordsByExperience() throws SQLException, IOException {
+    }
+
+    private void showTotalSalesValueOfEachManufacturer() throws SQLException, IOException {
+    }
+
+    private void showNMostPopularParts() throws SQLException, IOException {
     }
 }
